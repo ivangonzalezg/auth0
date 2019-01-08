f629b5e8602b084cec75b92b7ea0d647628736a3
ivangonzalezg <ivandario1998@gmail.com>
2019-01-08 11:37:25 -0500

    Added README.md

--- src/Auth.js
+++ src/Auth.js
@@ -8,7 +8,7 @@ export default class Auth {
     auth0 = new auth0.WebAuth({
         domain: "ivangonzalezg.auth0.com",
         clientID: "xOO4aLgj7i4FVtQYNJwtt0gDLpD5kyLZ",
-        redirectUri: "http://auth0-ig.herokuapp.com/callback",
+        redirectUri: "http://localhost:3000/callback",
         audience: "https://ivangonzalezg.auth0.com/userinfo",
         responseType: "token id_token",
         scope: "openid"
